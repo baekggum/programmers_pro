0ab965f9e966f9c234a4b996f17d209500512596
HyeonMin Kim <49273517+baekggum@users.noreply.github.com>
2023-09-06 15:26:33 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,16 @@
+function solution(s){
+    var answer = true;
+    var p_Num = 0;
+    var y_Num = 0;
+    
+    for(var i=0;i<s.length;i++){
+        if(s[i]=='p'||s[i]=='P')
+            p_Num+=1;
+        else if(s[i]=='y'||s[i]=='Y')
+            y_Num+=1;
+    }
+
+    answer =  p_Num == y_Num;
+    
+    return answer;
+}
