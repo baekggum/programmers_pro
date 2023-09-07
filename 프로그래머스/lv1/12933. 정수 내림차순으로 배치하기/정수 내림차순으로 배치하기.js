0b3cca349b4b8094f259150f6f0ec9d527154e63
HyeonMin Kim <49273517+baekggum@users.noreply.github.com>
2023-09-07 22:58:51 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -13,4 +13,14 @@ function solution(n) {
         answer+=i
     }
     return parseInt(answer);
+//     //숫자가 분명히 더 빠름
+//     var nums =[];
+//     do{
+//         nums.push(n%10);
+//         n=Math.floor(n/10);
+//     } while(n>0)
+
+//     return nums.sort((a,b)=>b-a).join('')*1;
+//     //문자는 느림
+//     return (n+"").split('').sort((a,b)=>b-a).join('')*1;
 }
