306accdf2eab1cb218af37a7dc99a49c92dd2332
HyeonMin Kim <49273517+baekggum@users.noreply.github.com>
2023-09-08 13:13:05 +0900

    [unrated] Title: 추억 점수, Time: 2.69 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,21 @@
+function solution(name, yearning, photo) {
+    var answer=[] 
+    var dict=[]
+    var num = 0;
+    for(var i=0;i<name.length;i++)
+        dict[name[i]]=yearning[i]
+    
+    console.log(photo.length)
+    for(var i=0;i<photo.length;i++){
+        for(var j=0;j<photo[i].length;j++){
+            if(dict[photo[i][j]]==undefined)
+                continue
+            num+=dict[photo[i][j]]
+        }
+        answer.push(num)
+        num=0
+    }
+
+    
+    return answer;
+}
